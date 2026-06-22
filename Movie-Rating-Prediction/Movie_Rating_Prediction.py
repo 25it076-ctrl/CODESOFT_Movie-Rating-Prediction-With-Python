842cd18997d509b6b32c378719640ce4c01a4f88
25it076-ctrl <25it076@rmd.ac.in>
2026-06-22 20:00:26 +0530

    Add files via upload

---
+++ Movie-Rating-Prediction/Movie_Rating_Prediction.py
@@ -0,0 +1,11 @@
+Genre,Director,Actor,Rating
+Action,Christopher Nolan,Christian Bale,8.8
+Drama,Frank Darabont,Tim Robbins,9.3
+Action,Jon Favreau,Robert Downey Jr,7.9
+Comedy,Todd Phillips,Bradley Cooper,7.7
+Drama,James Cameron,Kate Winslet,7.9
+Action,Christopher Nolan,Leonardo DiCaprio,8.7
+Comedy,Rajkumar Hirani,Aamir Khan,8.4
+Drama,Zoya Akhtar,Ranveer Singh,7.9
+Action,S.S. Rajamouli,Prabhas,8.2
+Drama,Sanjay Leela Bhansali,Ranveer Singh,7.5
